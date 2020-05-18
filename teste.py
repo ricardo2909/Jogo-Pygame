b9d2b90809c87af4b75962da4965576a2e3e0f80
Alexandre <alexandrea8@al.insper.edu.br>
2020-05-18 17:20:30 -0300

    Jogo 1

--- teste.py
+++ teste.py
@@ -1,2 +1,22 @@
-print('oi')
-print('teste2')
+import pygame
+
+pygame.init()
+
+WIDTH = 700
+HEIGHT = 600
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Crossy Froger')
+
+game = True
+
+while game:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            game = False
+
+    window.fill((255, 255, 255))
+
+    pygame.display.update()
+
+image = pygame.image.load('Pictures\Saved Pictures\ruajogo.jpg').convert()
+image = pygame.transform.scale(image, (125, 166))
