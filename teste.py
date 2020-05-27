354f8e135cb5da58ec6c528bc760e4712b563ff9
Alexandre <alexandrea8@al.insper.edu.br>
2020-05-27 14:13:24 -0300

    imagem

--- teste.py
+++ teste.py
@@ -11,12 +11,15 @@ game = True
 
 while game:
     for event in pygame.event.get():
-        if event.type==pygame.QUIT:
+        if event.type == pygame.QUIT:
             game = False
 
     window.fill((255, 255, 255))
+    cor = (255, 0, 0)
+    vertices = [(250, 0), (500, 200), (250, 400), (0, 200)]
+    pygame.draw.polygon(window, cor, vertices)
 
     pygame.display.update()
 
-image = pygame.image.load('Pictures\Saved Pictures\ruajogo.jpg').convert()
+image = pygame.image.load('imagem rua.jpg').convert()
 image = pygame.transform.scale(image, (125, 166))
