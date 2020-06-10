9f173f649e1c344e669e3cd11941e877e47bcfac
Alexandre <alexandrea8@al.insper.edu.br>
2020-06-10 15:24:06 -0300

    jogador

--- teste.py
+++ teste.py
@@ -22,10 +22,14 @@ carro_4 = pygame.image.load('car4.png').convert_alpha()
 carro_4 = pygame.transform.scale(carro_4, (car_WIDTH, car_HEIGHT))
 fox = pygame.image.load('Fox.png').convert_alpha()
 fox = pygame.transform.scale(fox, (90, 70))
-foguinho = pygame.image.load('animated fire.jpg').convert_alpha()
+foguinho = pygame.image.load('firee.jpg').convert_alpha()
 foguinho = pygame.transform.scale(foguinho, (80, 50))
 
+score_font = pygame.font.Font('font/PressStart2P.ttf', 28)
+
 lista_carro = [5, 70, 135, 200, 270, 335, 400, 465]
+
+lives = 3
 
 class cars1(pygame.sprite.Sprite):
     def __init__(self, img):
@@ -85,6 +89,7 @@ class raposa(pygame.sprite.Sprite):
         self.rect.y = 520
         self.speedx = 0
         self.speedy = 0
+        self.score = 0
 
     def update(self):
         self.rect.x += self.speedx
@@ -95,9 +100,10 @@ class raposa(pygame.sprite.Sprite):
             self.rect.y = 520
             self.speedx = 0
             self.speedy = 0
+            self.score += 150
 
 class fire(pygame.sprite.Sprite):
-    def __init__(self, img):
+    def __init__(self, img, jogador):
         pygame.sprite.Sprite.__init__(self)
 
         self.image = img
@@ -105,14 +111,17 @@ class fire(pygame.sprite.Sprite):
         self.rect.x = random.randint(1, 600)
         self.rect.y = random.choice(lista_carro)
         self.changed = pygame.time.get_ticks()
+        self.jogador = jogador
 
     def update(self):
         now = pygame.time.get_ticks()
 
-        if now - self.changed > 3000:
+        if now - self.changed > 5000:
             self.changed = now
             self.rect.x = random.randint(1, 600)
             self.rect.y = random.choice(lista_carro)
+            while self.rect.y == self.jogador.rect.y:
+                self.rect.y = random.choice(lista_carro)
         
 game = True
 
@@ -150,7 +159,7 @@ for i in range(1):
     all_cars3.add(carro4)
 
 for i in range(2):
-    fogos = fire(foguinho)
+    fogos = fire(foguinho, jogador)
     all_sprites.add(fogos)
     all_fogos.add(fogos)
 
@@ -167,8 +176,11 @@ while game:
             if event.key == pygame.K_RIGHT:
                 jogador.rect.x += 65
             if event.key == pygame.K_UP:
+                jogador.score += 50
                 jogador.rect.y -=65
             if event.key == pygame.K_DOWN:
+                if jogador.rect.y < 520:
+                    jogador.score -= 100
                 jogador.rect.y +=65
         
         if event.type == pygame.KEYUP:
@@ -185,31 +197,61 @@ while game:
 
     hits1 = pygame.sprite.spritecollide(jogador, all_cars1, True)
     if len(hits1) > 0:
-        game = False
-        print(1)
+        lives -= 1
+        jogador.rect.x = 300
+        jogador.rect.y = 520
+        jogador.score -= 200
+        if lives == 0:
+            game = False
     
     hits3 = pygame.sprite.spritecollide(jogador, all_cars3, True)
     if len(hits3) > 0:
-        game = False
-        print(3)
+        lives -= 1
+        jogador.rect.x = 300
+        jogador.rect.y = 520
+        jogador.score -= 200
+        if lives == 0:
+            game = False
     
     hits2 = pygame.sprite.spritecollide(jogador, all_cars2, True)
     if len(hits2) > 0:
-        game = False
-        print(2)
+        lives -= 1
+        jogador.rect.x = 300
+        jogador.rect.y = 520
+        jogador.score -= 200
+        if lives == 0:
+            game = False
     
     hits4 = pygame.sprite.spritecollide(jogador, all_cars4, True)
     if len(hits4) > 0:
-        game = False
-        print(4)
+        lives -= 1
+        jogador.rect.x = 300
+        jogador.rect.y = 520
+        jogador.score -= 200
+        if lives == 0:
+            game = False
 
     hits5 = pygame.sprite.spritecollide(jogador, all_fogos, True)
     if len(hits5) > 0:
-        game = False
-        print(5)
+        lives -= 1
+        jogador.rect.x = 300
+        jogador.rect.y = 520
+        jogador.score -= 200
+        if lives == 0:
+            game = False
 
-    window.fill((255, 255, 255))
+    window.fill((0, 0, 0))
     window.blit(backgroud, [0, 0])
     all_sprites.draw(window)
+
+    text_surface = score_font.render("{:08d}".format(jogador.score), True, (255, 255, 0))
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (150,  620)
+    window.blit(text_surface, text_rect)
+
+    text_surface = score_font.render(chr(9829) * lives, True, (255, 0, 0))
+    text_rect = text_surface.get_rect()
+    text_rect.bottomleft = (600, 640)
+    window.blit(text_surface, text_rect)
 
     pygame.display.update()
